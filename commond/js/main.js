bb54a2a88c914836defa21d84e3321932e2dc743
duck <1012746176@qq.com>
2015-11-29 20:53:31 +0800

    提交主页

--- commond/js/main.js
+++ commond/js/main.js
@@ -1,13 +1,71 @@
 /**
  * Created by Administrator on 15-11-20.
  */
-var mainApp = angular.module("mainApp",["ngRoute","regModule","LoginModule","userModule"]);
+var mainApp = angular.module("mainApp",["ngRoute","homeModule","aboutModule"]);
 
 mainApp.config(["$routeProvider",function($routeProvider){
-    $routeProvider.when("/reg",{templateUrl:"js/module1/reg.html",controller:"regController"});
-    $routeProvider.when("/login",{templateUrl:"js/module2/login.html",controller:"loginController"});
-    $routeProvider.when("/users",{templateUrl:"js/module3/user.html",controller:"userController"});
+    $routeProvider.when("/home",{templateUrl:"ccg_Home/home.html",controller:"homeController"});
+    $routeProvider.when("/about",{templateUrl:"zhaoyong_About/about.html",controller:"aboutController"});
+
     $routeProvider.otherwise({
-        redirectTo:"/reg"
+        redirectTo:"/home"
     })
 }]);
+
+
+mainApp.controller("navController",function($scope,$location){
+    $scope.homecolor="#000000";
+    $scope.aboutcolor = "";
+    $scope.servicescolor = "";
+    $scope.projectscolor = "";
+    $scope.clientscolor = "";
+    $scope.concactscolor = "";
+    $scope.clickhome = function(){
+        $scope.homecolor="#000000";
+        $scope.aboutcolor = "";
+        $scope.servicescolor = "";
+        $scope.projectscolor = "";
+        $scope.clientscolor = "";
+        $scope.concactscolor = "";
+    };
+    $scope.clickabout = function(){
+        $scope.homecolor="";
+        $scope.aboutcolor = "#000000";
+        $scope.servicescolor = "";
+        $scope.projectscolor = "";
+        $scope.clientscolor = "";
+        $scope.concactscolor = "";
+    };
+    $scope.clickservices = function(){
+        $scope.homecolor="";
+        $scope.aboutcolor = "";
+        $scope.servicescolor = "#000000";
+        $scope.projectscolor = "";
+        $scope.clientscolor = "";
+        $scope.concactscolor = "";
+    }
+    $scope.clickprojects = function(){
+        $scope.homecolor="";
+        $scope.aboutcolor = "";
+        $scope.servicescolor = "";
+        $scope.projectscolor = "#000000";
+        $scope.clientscolor = "";
+        $scope.concactscolor = "";
+    }
+    $scope.clickclients = function(){
+        $scope.homecolor="";
+        $scope.aboutcolor = "";
+        $scope.servicescolor = "";
+        $scope.projectscolor = "";
+        $scope.clientscolor = "#000000";
+        $scope.concactscolor = "";
+    }
+    $scope.clickconcacts = function(){
+        $scope.homecolor="";
+        $scope.aboutcolor = "";
+        $scope.servicescolor = "";
+        $scope.projectscolor = "";
+        $scope.clientscolor = "";
+        $scope.concactscolor = "#000000";
+    }
+});
